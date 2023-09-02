d5c4de1ad26a38ed4db7798cdbc368b401e0b250
Waihonglim94 <36778407+JonasanL@users.noreply.github.com>
2023-09-02 13:20:44 +0800

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React mo**** f*****
         </a>
       </header>
     </div>
