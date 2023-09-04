57fad96d9b9ea3e5f6f9b6eb6d3c8d77dd8917a3
Waihonglim94 <waihonglim@icloud.com>
2023-09-04 14:24:29 +0800

    left side adding video link

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import WorkList from './WorkList'; // Import your component
 
 const works= [
   {
-    "name":"work1",
-    "url":process.env.PUBLIC_URL + "Videos/test1.mp4",
+    "name":"Zenmode app",
+    // "url":process.env.PUBLIC_URL + "Videos/test1.mp4",
+    "url":"https://www.youtube.com/watch?v=h6oGqvoWIks&ab_channel=OnePlus",
+    "year":"2020",
+    "isYoutube":true
   },
   
   {
-    "name":"work2",
+    "name":"OnePlus x Hasselblad Camera app",
     "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-}  
+    "year":"2020-2021",
+    "isYoutube":false
+  },
+  
+  {
+    "name":"Nothing X",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  },
+  
+  {
+    "name":"Camera app",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  },
+  
+  {
+    "name":"OS_Boot",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  },
+  
+  {
+    "name":"OS_Folders",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  },
+  
+  {
+    "name":"OS_Settings",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  },
+  
+  {
+    "name":"motion workshop",
+    "url":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "year":"2023"
+  }
 ]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Waihong</h1> 
+        <h1>WaiHong</h1> 
       </header>
 
       
       <WorkList works={works} />  
-      {/* <VideoDisplay works={works} /> */}
     
     </div>
   );
--- src/WorkList.js
+++ src/WorkList.js
@@ -1,24 +1,132 @@
+// // WorkList.js
+// import React, { useState } from 'react';
+// import VideoJS from './Videojs' 
+// import videojs from 'video.js';
+// import YouTube from 'react-youtube';
+
+
+// function WorkList({ works, selectedWork, onSelectWork }) {
+//     // function onSelectWork(url){
+//     //     console.log(url)
+//     // }
+//     const [selectedVideoUrl, setSelectedVideoUrl] = useState(null); 
+//     const [hoveredWork, setHoveredWork] = useState(null);
+
+//     const playerRef = React.useRef(null);
+
+//     const handleItemClick = (videoUrl, index) => {
+//       setSelectedVideoUrl(videoUrl);
+//       setHoveredWork(index); // Set hovered item when clicked
+//     };
+  
+//     const handleMouseEnter = (index) => {
+//       setHoveredWork(index);
+//     };
+  
+//     const handleMouseLeave = () => {
+//       setHoveredWork(null); // Clear hovered item on mouse leave
+//     };
+
+//   const youtubeVideoId = "https://www.youtube.com/watch?v=h6oGqvoWIks&ab_channel=OnePlus";
+//   const videoJsOptions = {
+//     autoplay: true,
+//     controls: false,
+//     responsive: true,
+//     fluid: true,
+//     sources: selectedVideoUrl ? [ { src: selectedVideoUrl, type: 'video/mp4', }, ] : [],
+//   };
+
+//   const handlePlayerReady = (player) => {
+//     playerRef.current = player;
+
+//     // You can handle player events here, for example:
+//     player.on('waiting', () => {
+//       videojs.log('player is waiting');
+//     });
+
+//     player.on('dispose', () => {
+//       videojs.log('player will dispose');
+//     });
+//   };
+
+//   return (
+//     <div>
+//         {/* <ul class="custom-list" className="work-list"> */}
+//         <ul class="custom-list">
+//         {works.map((work, index) => (
+//           <li
+//             key={index}
+//             className={`work-item ${selectedWork === index ? 'selected' : ''} ${
+//               hoveredWork === index ? 'hovered' : ''
+//             }`}
+//             onClick={() => handleItemClick(work.url, index)}
+//             onMouseEnter={() => handleMouseEnter(index)}
+//             onMouseLeave={handleMouseLeave}
+//           >
+//             {work.name}
+//             {hoveredWork === index && (
+//               <div className="year-popup">{work.year}</div>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+
+
+//         {selectedVideoUrl && 
+//         ( <div> 
+//             {/* <VideoJS options={videoJsOptions} onReady={handlePlayerReady} /> */}
+//             {work.isYoutube ? (
+//               <YouTube videoId={youtubeVideoId} />
+//             ) : (
+//               <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+//             )}
+
+//         </div> )}
+//     </div>
+
+//   );
+// }
+
+// //trying to get credential test test test
+// export default WorkList;
+
+
 // WorkList.js
 import React, { useState } from 'react';
 import VideoJS from './Videojs' 
 import videojs from 'video.js';
+import YouTube from 'react-youtube';
 
 
 function WorkList({ works, selectedWork, onSelectWork }) {
     // function onSelectWork(url){
     //     console.log(url)
     // }
-    const [selectedVideoUrl, setSelectedVideoUrl] = useState(null); 
-    const handleItemClick = (videoUrl) => { setSelectedVideoUrl(videoUrl); };
+    const [selectedVideo, setSelectedVideo] = useState(null); 
+    const [hoveredWork, setHoveredWork] = useState(null);
 
     const playerRef = React.useRef(null);
 
+    const handleItemClick = (work, index) => {
+      setSelectedVideo(work);
+      setHoveredWork(index); // Set hovered item when clicked
+    };
+  
+    const handleMouseEnter = (index) => {
+      setHoveredWork(index);
+    };
+  
+    const handleMouseLeave = () => {
+      setHoveredWork(null); // Clear hovered item on mouse leave
+    };
+
+  // const youtubeVideoId = "https://www.youtube.com/watch?v=h6oGqvoWIks&ab_channel=OnePlus";
   const videoJsOptions = {
     autoplay: true,
     controls: false,
     responsive: true,
     fluid: true,
-    sources: selectedVideoUrl ? [ { src: selectedVideoUrl, type: 'video/mp4', }, ] : [],
+    sources: selectedVideo ? [ { src: selectedVideo.url, type: 'video/mp4', }, ] : [],
   };
 
   const handlePlayerReady = (player) => {
@@ -36,25 +144,35 @@ function WorkList({ works, selectedWork, onSelectWork }) {
 
   return (
     <div>
-        <ul className="work-list">
+        {/* <ul class="custom-list" className="work-list"> */}
+        <ul class="custom-list">
         {works.map((work, index) => (
-            <li
+          <li
             key={index}
-            className={selectedWork === index ? 'selected' : ''}
-            onClick={() => handleItemClick(work.url)}
-            
-            //   onMouseEnter={() => onSelectWork(index)}
-            //   onMouseLeave={() => onSelectWork(null)}
-            >
+            className={`work-item ${selectedWork === index ? 'selected' : ''} ${
+              hoveredWork === index ? 'hovered' : ''
+            }`}
+            onClick={() => handleItemClick(work, index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          >
             {work.name}
-            </li>
+            {hoveredWork === index && (
+              <div className="year-popup">{work.year}</div>
+            )}
+          </li>
         ))}
-        </ul>
+      </ul>
 
-        {selectedVideoUrl && 
+
+        {selectedVideo && 
         ( <div> 
-            <h3>Video Player</h3> 
-            <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+            {console.log(selectedVideo.name)}
+            {selectedVideo.isYoutube ? (
+              <YouTube videoId={"https://youtu.be/YhdzE35Z6as?si=s2Ad2OhuR84dMw6q"} />
+            ) : (
+              <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+            )}
 
         </div> )}
     </div>
